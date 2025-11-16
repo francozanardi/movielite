5e35c8905cb4b3d53b289fe667842360ae53e68e
Franco Zanardi <francozanardi.97@gmail.com>
2025-11-15 21:06:53 -0300

    chore: bump version to 0.2.0

--- src/movielite/__init__.py
+++ src/movielite/__init__.py
@@ -21,7 +21,7 @@ from .enums import VideoQuality
 from .logger import get_logger, set_log_level
 from . import vfx, afx, vtx
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 __all__ = [
     "MediaClip",
